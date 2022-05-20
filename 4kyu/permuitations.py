6e2724c5b31732a0e340a78b6c7c1e9708cd1233
GitHub-Joayo <dkssudxx@naver.com>
2022-05-20 23:32:47 +0900

    Update solution
    Upgrade permutation solution which is not use built-in module.
    I solved it recursively, But I'm still confusing.
    (I think previous solution is cheating...)

--- 4kyu/permuitations.py
+++ 4kyu/permuitations.py
@@ -1,7 +1,19 @@
-from itertools import permutations as p
-
 def permutations(string):
-    return [ "".join(each) for each in set(p(string, len(string))) ]
+    result = set()
+    if len(string) > 1:
+        for idx in range(len(string)):
+            for ch in permutations(string[0:idx] + string[idx + 1:]):
+                result.add(string[idx] + ch)
+    else:
+        result = [string]
+        
+    return list(result)
+
+
+# from itertools import permutations as p
+
+# def permutations(string):
+#     return [ "".join(each) for each in set(p(string, len(string))) ]
 
 
 if __name__ == "__main__":
